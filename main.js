a306d30756a6566766be90c414bdca252a35477d
Nika Jobava <trollgamer481@gmail.com>
2021-08-04 20:36:54 +0400

    add word btn works

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const addWordBtn = document.querySelector(".add");
+const removeWordBtn = document.querySelector(".remove");
+
+const wordsArr = [];
+
+addWordBtn.addEventListener("click", function () {
+    let addedWord = prompt("add word");
+    addedWord = addedWord.split(" ")
+    wordsArr.push(addedWord);
+    console.log(wordsArr);
+    localStorage["allWords"] = JSON.stringify([...wordsArr]);
+    console.log(JSON.parse(localStorage["allWords"]));
+});
